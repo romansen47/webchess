45cb69e742730b6c705fdcd6f37241342648659b
Ro Mansen <romansen47@gmail.com>
2019-01-10 09:46:58 +0100

    configuration steps moved from Dockerfile to run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-chown -R www-data:www-data /var/www/webchess
+cd /tmp && git clone https://github.com/Thorium/webchess
+
+mv /tmp/webchess/* /var/www/
+
+chown -R www-data:www-data /var/www/
 
 service mysql start
 
 mysql -u root < /tmp/mysql
+
+echo ServerName webchess >> /etc/apache2/apache2.conf
 
 /usr/sbin/apache2ctl -D FOREGROUND
